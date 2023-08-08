3536f47cb7fa05a475df42c173e5f4677bd77c4f
Alessandro Caliaro <alessandro.caliaro@soluzione1.it>
2023-08-08 10:25:59 +0200

    Aggiungere i file di progetto.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppDebugProblemiOS;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppDebugProblemiOS;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
